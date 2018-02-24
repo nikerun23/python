da932d3e0211fe97ad196591b055d370e89e4d66
HK Lee <nikerun23@naver.com>
2018-02-24 16:51:08 +0900

    인프런 로그인 후 웹크롤링 요청

---
+++ 20180224/inflearnLoginTest.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = 'https://www.inflearn.com/wp-login.php'
+
+s = requests.Session()
+
+s.post(url, data={'log': 'nikerun23',
+                  'pwd': 'wlsldjtm23'
+                  }
+       )
+
+res = s.get('https://www.inflearn.com/')
+soup = bs(res.text, 'lxml')
+name = soup.select_one('ul.topmenu > li:nth-of-type(1) > a > span')
+print(name.text)
