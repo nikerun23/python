d2ad68f81b968a11a65e45c516313b62e6d7b885
HK Lee <nikerun23@naver.com>
2018-02-26 17:31:20 +0900

    csv정보를 dict타입의 List객체로 저장

--- rnd_crawler/csvReadTest.py
+++ rnd_crawler/csvReadTest.py
@@ -1,10 +1,20 @@
 import csv
 
 csv_reader = csv.DictReader(open('csv/url_list.csv', encoding='UTF8'))
-print(csv_reader.fieldnames)
 
-for index, h in enumerate(csv_reader.fieldnames):
+urlFieldNames = csv_reader.fieldnames
+urlDicList = []
+urlDic = {}
+
+print(urlFieldNames)
+for index, h in enumerate(urlFieldNames):
     print(index,h)
 
 for row in csv_reader.reader:
-    print(row[2])
+    for index, h in enumerate(urlFieldNames):
+        urlDic[h] = row[index]
+
+    urlDicList.append(urlDic)
+
+print(len(urlDicList))
+print(urlDicList[7])
