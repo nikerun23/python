2ba0ba039fe86d66e1795583c7ec298f389be0e4
HK Lee <nikerun23@naver.com>
2018-02-26 15:32:52 +0900

    cURL을 통한 Session에 헤더정보 주입 후 크롤링 시작

--- rnd_crawler/rndBoardReadcURLTest.py
+++ rnd_crawler/rndBoardReadcURLTest.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = 'https://www.nrf.re.kr/tender/list?menu_no=57'
+url = 'https://www.kista.re.kr/usr/com/prm/BBSList.do?bbsId=BBSMSTR_000000000203&menuNo=10000'
 selectTR = 'div.board_list tbody > tr'
 selectTitle = 'td a.ntsviewBtn'
 selectDate = 'td:nth-of-type(5)'
@@ -12,37 +12,40 @@ headers = {
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Referer': 'https://www.nrf.re.kr/index',
+    'Referer': 'https://www.kista.re.kr',
     'Connection': 'keep-alive',
 }
 
 ss = requests.Session()
 ss.headers = headers
 res = ss.get(url)
+print(ss.cookies)
+print(res.text)
 # res = requests.get(url)
 # res.encoding = 'utf-8'
 # res.encoding = 'euc-kr'
 # print(res.text)
-soup = bs(res.text, 'lxml')
-boardList = soup.select(
-    selectTR
-)
-# print(boardList)
-for i in boardList:
-
-    title = ''
-    dateStr = ''
-    boardNo = ''
-    titleList = i.select_one(selectTitle)
-    if titleList is not None:
-        title = titleList.text
-    else:
-        title = '<제목 정보를 읽어올 수 없습니다>'
-    boardNo = ''
-    dateList = i.select_one(selectDate)
-    if dateList is not None:
-        dateStr = dateList.text
-    else:
-        dateStr = '<날짜 정보를 읽어올 수 없습니다>'
-    print(boardNo, title, dateStr)
-
+#
+# soup = bs(res.text, 'lxml')
+# boardList = soup.select(
+#     selectTR
+# )
+# # print(boardList)
+# for i in boardList:
+#
+#     title = ''
+#     dateStr = ''
+#     boardNo = ''
+#     titleList = i.select_one(selectTitle)
+#     if titleList is not None:
+#         title = titleList.text
+#     else:
+#         title = '<제목 정보를 읽어올 수 없습니다>'
+#     boardNo = ''
+#     dateList = i.select_one(selectDate)
+#     if dateList is not None:
+#         dateStr = dateList.text
+#     else:
+#         dateStr = '<날짜 정보를 읽어올 수 없습니다>'
+#     print(boardNo, title, dateStr)
+#
