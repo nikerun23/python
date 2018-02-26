fe1dfd65f4e78c0c54e894f0b587426d3aa3dcc8
HK Lee <nikerun23@naver.com>
2018-02-26 14:39:10 +0900

    select_one함수로 변경, 예외처리 추가

--- boardReadTest.py
+++ boardReadTest.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup as bs
 
 csvDict = {'url': 'http://www.mof.go.kr/article/list.do?boardKey=9&currentPageNo=1&menuKey=375&recordCountPerPage=10&searchEtc1=&searchEtc2=&searchEtc3=&searchEtc4=&searchEtc5=&searchDeptName=&searchStartDate=&searchEndDate=&searchSelect=title&searchValue='
             , 'selectTR': 'div.board_body > table.board_list > tbody > tr'
-            , 'selectTitle': 'td.tx_al > a'
-            , 'selectDate': 'td:nth-of-type(5)'
+            , 'selectTitle': 'td.tx_al > a.2'
+            , 'selectDate': 'td:nth-of-type(10)'
             ,'linkUrl': 'http://www.mof.go.kr/article/view.do?articleKey=18821&searchSelect=title&boardKey=9&menuKey={boardNo}&currentPageNo=1'}
 
 url = csvDict['url']
@@ -26,11 +26,14 @@ for i in boardList:
     title = ''
     dateStr = ''
     boardNo = ''
-    titleList = i.select(selectTitle)
-    title = titleList[0].text
+    titleList = i.select_one(selectTitle)
+    if titleList is not None:
+        title = titleList.text
     boardNo = ''
-    dateList = i.select(selectDate)
-    dateStr = dateList[0].text
+
+    dateList = i.select_one(selectDate)
+    if dateList is not None:
+        dateStr = dateList.text
 
     print(boardNo, title, dateStr,'\n',linkUrl.format(boardNo=boardNo))
 
