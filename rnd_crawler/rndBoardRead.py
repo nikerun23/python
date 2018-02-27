a39a27f664c6e0206b6c134bc7ead4c47a19f909
HK Lee <nikerun23@naver.com>
2018-02-27 16:41:04 +0900

    변수명 표현식에 맞게 수정

--- rnd_crawler/rndBoardRead.py
+++ rnd_crawler/rndBoardRead.py
@@ -6,108 +6,105 @@ import datetime
 # +++++++++++ Util Area start +++++++++++++++++++++++++++++++++
 def getYesterdayList():
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
-    dayOfWeek = ['월', '화', '수', '목', '금', '토', '일']
-    yesterdayList = [yesterday]
+    day_of_week = ['월', '화', '수', '목', '금', '토', '일']
+    yesterday_list = [yesterday]
 
     # yesterday가 일요일 경우 전 주 금요일까지 조회
-    if '일' == dayOfWeek[yesterday.weekday()]:
-        yesterdayList.append(yesterday - datetime.timedelta(days=1))
-        yesterdayList.append(yesterday - datetime.timedelta(days=2))
+    if '일' == day_of_week[yesterday.weekday()]:
+        yesterday_list.append(yesterday - datetime.timedelta(days=1))
+        yesterday_list.append(yesterday - datetime.timedelta(days=2))
 
-    print('전일 :', yesterday, dayOfWeek[yesterday.weekday()])
-    print('크롤링 날짜 :', yesterdayList)
+    print('전일 :', yesterday, day_of_week[yesterday.weekday()])
+    print('크롤링 날짜 :', yesterday_list)
     print('-----------------------------------------------------------------------------------')
-    return yesterdayList
+    return yesterday_list
 
-def yesterdayCheck(dayList, boardDate):
-    if boardDate is None:
+def yesterdayCheck(day_list, board_date):
+    if board_date is None:
         return False
     result = False
-    for yesterday in dayList:
-        if yesterday == boardDate:
+    for yesterday in day_list:
+        if yesterday == board_date:
             result = True
     return result
 
-def validDate(dateStr):
+def validDate(date_str):
     """날짜를 검증합니다"""
-    dateStr = dateStr.strip()
-    if dateStr in ('', None):
+    date_str = date_str.strip()
+    if date_str in ('', None):
         return None
-    dateStr = dateStr.replace(' ', '').replace(',', '-').replace('.', '-').replace('/', '-')
+    date_str = date_str.replace(' ', '').replace(',', '-').replace('.', '-').replace('/', '-')
 
-    if dateStr[-1] == '-':
-        dateStr = dateStr[:-1]
+    if date_str[-1] == '-':
+        date_str = date_str[:-1]
 
     # datetime 객체로 변환
-    dateTimeStr = datetime.datetime.strptime(dateStr, '%Y-%m-%d')
-    dateType = datetime.date(dateTimeStr.year, dateTimeStr.month, dateTimeStr.day)
+    date_time_str = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    result = datetime.date(date_time_str.year, date_time_str.month, date_time_str.day)
+    return result
 
-    return dateType
-
-def validTitle(titleStr):
+def validTitle(title_str):
     """글제목을 검증합니다"""
-    titleStr = titleStr.strip()
-    titleStr = titleStr.replace('  ', '').replace('\t', '').replace('\n', '')
-    return titleStr
+    title_str = title_str.strip()
+    title_str = title_str.replace('  ', '').replace('\t', '').replace('\n', '')
+    return title_str
 
 def csvReadUrl(src):
 
     csv_reader = csv.DictReader(open(src, encoding='UTF8'))
-    urlFieldNames = csv_reader.fieldnames
-    urlDictList = []
+    url_field_names = csv_reader.fieldnames
+    url_dict_list = []
 
     for row in csv_reader.reader:
-        urlDict = {}
+        url_dict = {}
         if 'X' == row[7]:  # Crawler
             continue
-        for index, h in enumerate(urlFieldNames):
-            urlDict[h] = row[index].strip()
-        urlDictList.append(urlDict)
-    return urlDictList
+        for index, h in enumerate(url_field_names):
+            url_dict[h] = row[index].strip()
+        url_dict_list.append(url_dict)
+    return url_dict_list
 
 
 # +++++++++++ Util Area end +++++++++++++++++++++++++++++++++
-rowNum = 10 - 2 # index 값 보정
-urlDictList = csvReadUrl('csv/url_list.csv')
-print(urlDictList[rowNum])
-yesterdayList = getYesterdayList()
+row_num = 10 - 2 # index 값 보정
+url_dict_list = csvReadUrl('csv/url_list.csv')
+print(url_dict_list[row_num])
+yesterday_list = getYesterdayList()
 
-def printRnD(csvInfo):
-    print(csvInfo['부처'],'---',csvInfo['기관'],'---------------------------------------')
-    print(csvInfo['URL'])
-    url = csvInfo['URL']
-    selectTR = csvInfo['TR']
-    selectTitle = csvInfo['Title']
-    selectDate = csvInfo['Date']
-    etcStr = csvInfo['etc']
+def printRnD(csv_info):
+    print(csv_info['부처'], '---', csv_info['기관'], '---------------------------------------')
+    print(csv_info['URL'])
+    url = csv_info['URL']
+    select_tr = csv_info['TR']
+    select_title = csv_info['Title']
+    select_date = csv_info['Date']
+    etc_str = csv_info['etc']
 
     req = requests.get(url)
-    if 'utf-8' == etcStr:
+    if 'utf-8' == etc_str:
         req.encoding = 'utf-8'
-    elif 'euc-kr' == etcStr:
+    elif 'euc-kr' == etc_str:
         req.encoding = 'euc-kr'
 
     html = req.text
     # print(html)
     soup = bs(html, 'lxml')
-    boardList = soup.select(
-        selectTR
+    board_list = soup.select(
+        select_tr
     )
     # print(boardList)
-    for i in boardList:
-        title = ''
-        boardNo = ''
-        titleList = i.select_one(selectTitle)
-        title = validTitle(titleList.text)
-        boardNo = ''
-        dateList = i.select_one(selectDate)
-        boardDate = validDate(dateList.text) # datetime객체로 반환
+    for i in board_list:
+        title_list = i.select_one(select_title)
+        title = validTitle(title_list.text)
+        board_no = ''
+        date_list = i.select_one(select_date)
+        board_date = validDate(date_list.text) # datetime객체로 반환
         # 전일 공고만 출력
-        if yesterdayCheck(yesterdayList, boardDate):
-            print(boardNo, title, boardDate)
+        if yesterdayCheck(yesterday_list, board_date):
+            print(board_no, title, board_date)
     print('-----------------------------------------------------------------------')
 
-for index, info in enumerate(urlDictList):
+for index, info in enumerate(url_dict_list):
     print('csv Row Num :',index + 2)
     printRnD(info)
 
