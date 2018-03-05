e8120c362279530b0afad5e464572be49d22d493
HK Lee <nikerun23@naver.com>
2018-03-05 13:30:06 +0900

    Selenium을 활용한 ajax사이트 웹크롤링

--- rnd_crawler/SeleniumTest.py
+++ rnd_crawler/SeleniumTest.py
@@ -3,13 +3,15 @@ from selenium import webdriver
 driver = webdriver.Chrome('./chromedriver')
 
 url = 'http://www.nipa.kr/board/boardList.it?boardNo=102&menuNo=31&page=1'
-click_css = '#container > div.leftWrap > div.snb > ul > li:nth-child(2) > a'
+click_css = {'#container > div.leftWrap > div.snb > ul > li:nth-of-type(2) > a',
+             '#container > div.leftWrap > div.snb > ul > li:nth-of-type(4) > a'}
 select_tr = '#q_content table > tbody > tr'
-select_title = 'td.title  a'
+select_title = 'td.title a'
 select_date = 'td.date'
 
 driver.get(url)
-driver.find_element_by_css_selector(click_css).click()
+for click in click_css:
+    driver.find_element_by_css_selector(click).click()
 board_list = driver.find_elements_by_css_selector(select_tr)
 
 for tr in board_list:
