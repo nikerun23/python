26b171fea381dd723cfaf3064bd1ce2f1478fd6f
HK Lee <nikerun23@naver.com>
2018-03-30 17:25:54 +0900

    코드 테스트파일 경로이동

---
+++ rnd_crawler/test_code.py
@@ -0,0 +1,3 @@
+import cx_Oracle
+
+con = cx_Oracle.connect('state_dev/dhQkWkd8@112.216.140.126:1522/orcl')
--- test_code.py
+++
@@ -1,5 +0,0 @@
-import telegram
-
-bot = telegram.Bot(token='594957094:AAG2amlQoS-enenuId2brtRhN4aXqLJH0bw')
-result_text = '안녕하세요 !!'
-bot.sendMessage(chat_id=568182246, text=result_text)
