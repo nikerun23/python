bef85d38e1ab201b6e0f73194f8bb3ace97d847d
HK Lee <nikerun23@naver.com>
2018-03-30 17:09:02 +0900

    예외처리 추가

--- rnd_crawler/rndBoardReadTest.py
+++ rnd_crawler/rndBoardReadTest.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = 'https://www.nrf.re.kr/tender/list?menu_no=57'
-select_tr = 'div.board_list tbody > tr'
-select_title = 'td a.ntsviewBtn'
-select_date = 'td:nth-of-type(5)'
+url = 'http://www.kfi.or.kr/home/brd/brd230/brd230_Type4_Lis.do?board_seq=14'
+select_tr = 'li.pd_t4 table.board_tb tr'
+select_title = 'td:nth-of-type(2) a'
+select_date = 'td:nth-of-type(3)'
 
-req = requests.get(url, verify=False)
+req = requests.get(url)
+# req = requests.get(url, verify=False)
 # req.encoding = 'utf-8'
 # req.encoding = 'euc-kr'
 html = req.text
@@ -18,12 +19,17 @@ board_list = soup.select(
 
 # print(board_list)
 for i in board_list:
+    try:
+        title_list = i.select_one(select_title)
+        title = title_list.text
+        board_no = ''
+        date_list = i.select_one(select_date)
+        date_str = date_list.text.strip()
 
-    title_list = i.select_one(select_title)
-    title = title_list.text
-    board_no = ''
-    date_list = i.select_one(select_date)
-    date_str = date_list.text.strip()
+        print(board_no, title, date_str)
+    except AttributeError:
+        print('AttributeError !!')
+        pass
 
-    print(board_no, title, date_str)
+
 
