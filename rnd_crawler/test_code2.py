636d9a7a0e07af63605e72794854866f5e6db220
HK Lee <nikerun23@naver.com>
2018-04-17 11:13:04 +0900

    파일 다운로드 및 respose 헤더 인코딩 문제

--- rnd_crawler/test_code2.py
+++ rnd_crawler/test_code2.py
@@ -2,9 +2,45 @@
 import rnd_crawler.utility as util
 import re
 from bs4 import BeautifulSoup as bs
+import requests
+from urllib.request import urlretrieve
+from urllib.request import urlopen
+import os
+import cgi
 
-html = "<a>해양수산부 <script>수립 연구 용역 입찰공고</script> </a>"
-soup = bs(html, 'lxml')
-soup.script.extract()
-print(soup)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+}
 
+url = 'http://www.mof.go.kr/jfile/readDownloadFile.do?fileId=MOF_ARTICLE_19574&fileSeq=1'
+
+print('url :', url)
+response = requests.get(url, stream=True, headers=headers)
+
+download_path = 'files/2018-04-16/해양수산부/'
+filename = 'TEST.hwp'
+file_path = download_path + filename
+
+directory = os.path.dirname(file_path)  # 폴더경로만 반환한다
+if not os.path.exists(directory):
+    os.makedirs(directory, exist_ok=True)  # exist_ok=True 상위 경로도 생성한다
+
+# urlretrieve(url, file_path)
+response = urlopen(url)
+csv = response.read()
+print(response)
+print(csv)
+response.encoding = 'utf-8'
+_, params = cgi.parse_header(response.headers.get('Content-Disposition', ''))
+filename = params
+
+print(filename)
+
+# f = open(filename, "wb")
+# for chunk in response.iter_content(chunk_size=1024):
+#     if chunk:
+#         f.write(chunk)
+
+
+# filename = re.findall("[^/]*$", url)[0]
+# print('filename :', filename)
