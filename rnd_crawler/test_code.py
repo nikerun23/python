af77d481d71d4ec98fc7a7cffee3653513d5ea0e
HK Lee <nikerun23@naver.com>
2018-04-23 10:37:56 +0900

    본문 내용에서 마감일 추출하여 정제하는 테스트 코드

--- rnd_crawler/test_code.py
+++ rnd_crawler/test_code.py
@@ -21,6 +21,29 @@ import re
 # body_text = "- 제출마감일: '18.4.27(금), 18:00"
 body_text = "제출기한 : 2018. 04. 28. (토) 14:00까지"
 
+
+# """" 공고 시작일, 마감일을 정제하여 반환합니다 """
+def valid_start_end_date(date_type, date_str, content_DateFormat):
+    # date_str = date_str.replace('\n','')
+    if re.match('(.*)[0-9]+', date_str, re.DOTALL) is None:  # 숫자가 없으면 return ''
+        print('########## 숫자가 없습니다 :',date_str)
+        return ''
+    date_str = date_str.strip().replace('.','-').replace('/','-')
+    date_str = re.sub('[^0-9~/시:\s-]', '', date_str)  # 2017-12-29~2018-01-03
+    if 'YYYY-MM-DD~YYYY-MM-DD' == content_DateFormat:
+        if 2 == date_type:  # content_StartDate : 2
+            date_str = date_str[:date_str.find('~')]
+        elif 3 == date_type:  # content_EndDate : 3
+            year_str = date_str[:date_str.find('-')].strip()
+            date_str = date_str[date_str.find('~')+1:]
+            if year_str not in date_str:  # 마감일에 연도 없을 경우 시작일의 연도를 붙여준다
+                date_str = year_str + '-' + date_str.strip()
+    print('date_str :',date_str, '시작일' if date_type == 2 else '마감일')
+    print('result :', util.valid_date(date_str, None).strftime('%Y-%m-%d'))
+    return util.valid_date(date_str, None).strftime('%Y-%m-%d')
+
+# valid_start_end_date()
+
 end_date_index = body_text.find('마감')
 print('end_date_index :',end_date_index)
 
