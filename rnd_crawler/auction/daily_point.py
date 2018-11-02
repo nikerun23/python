bcf9f55274bb3eabfe9160cebf517707e27dd91b
Lee Hyunkeun <nikerun23@naver.com>
2018-11-02 17:02:47 +0900

    옥션, 지마켓 매일포인트 자동 적립하는 크롤링 개발

--- rnd_crawler/auction/daily_point.py
+++ rnd_crawler/auction/daily_point.py
@@ -6,12 +6,12 @@ import telegram
 def daily_point(login_id, login_pw):
     print(login_id, login_pw, ' RUN !!!')
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')  # 헤드리스모드
+    #options.add_argument('--headless')  # 헤드리스모드
     options.add_argument('--disable-gpu')  # 호환성용 (필요없는 경우도 있음)
     options.add_argument('--window-size=1920x1080')  # (가상)화면 크기 조절
     options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(chrome_options=options)
-    # driver = webdriver.Chrome('./chromedriver')
+    # driver = webdriver.Chrome('chromedriver')
     driver.set_page_load_timeout(60)  # selenium timeout 60초
     login_url = 'https://memberssl.auction.co.kr/Authenticate/default.aspx?url=http%3A//promotion.auction.co.kr/promotion/MD/eventview.aspx%3FtxtMD%3D05F804C1E8'
     try:
@@ -52,6 +52,40 @@ def daily_point(login_id, login_pw):
         print(e)
     finally:
         driver.close()
+        driver.quit()
+
+
+def gmarket_daily_point(login_id, login_pw):
+    print(login_id, login_pw, ' RUN !!!')
+    options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')  # 헤드리스모드
+    options.add_argument('--disable-gpu')  # 호환성용 (필요없는 경우도 있음)
+    options.add_argument('--window-size=1920x1080')  # (가상)화면 크기 조절
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(chrome_options=options)
+    # driver = webdriver.Chrome('chromedriver')
+    driver.set_page_load_timeout(60)  # selenium timeout 60초
+    login_url = 'https://signinssl.gmarket.co.kr/login/login?prmtdisp=Y&url=http://promotion.gmarket.co.kr/Event/PlusZone.asp'
+    try:
+        driver.get(url=login_url)
+
+        time.sleep(5)
+        driver.find_element_by_css_selector('#id').send_keys(login_id)
+        time.sleep(1)
+        driver.find_element_by_css_selector('#pwd').send_keys(login_pw)
+        time.sleep(1)
+        driver.find_element_by_css_selector('#mem_login div.btn-login > a').click()
+        time.sleep(5)
+        driver.get(url="http://promotion.gmarket.co.kr/Event/AttendRoulette_none.asp")
+        time.sleep(5)
+        driver.find_element_by_css_selector('#wrapper a.button_start').click()
+        time.sleep(5)
+
+    except Exception as e:
+        print(e)
+    finally:
+        driver.close()
+        driver.quit()
 
 
 if '__main__' == __name__:
@@ -59,11 +93,20 @@ if '__main__' == __name__:
     # 텔레그램 Bot 메세지 보내기
     my_token = '602824143:AAEjqPKSe95ncMH9lDluEKwR_J7BorJUbWE'  # 토큰을 변수에 저장합니다.
     bot = telegram.Bot(token=my_token)  # bot을 선언합니다.
+    print(bot)
     updates = bot.getUpdates()
+    print(updates)
+    for u in updates:
+        print(u.message)
 
     # chat_id = bot.getUpdates()[-1].message.chat.id
     chat_id = '568182246'
-    bot.sendMessage(chat_id=chat_id, text='TEST')
+    print(chat_id)
+    #bot.sendMessage(chat_id=chat_id, text='TEST')
+
+    # 지마켓 포인트
+    gmarket_daily_point('', '')
+    gmarket_daily_point('', '')
 
     # 옥션 포인트
     daily_point('','')
